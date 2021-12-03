262e4edac51c905a2f04a16942305946b8342c8d
Shyrtle <joshyxboy@gmail.com>
2021-12-03 15:40:20 -0800

    base functionality

--- games/models.py
+++ games/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='games/')
+    gif = models.ImageField(upload_to='game_gifs/')
+    location = models.CharField(max_length=200)
--- games/views.py
+++ games/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Game
 
 # Create your views here.
+def home(request):
+    games = Game.objects.all()
+    ctx = {'games': games}
+    return render(request, 'games/home.html', ctx)
